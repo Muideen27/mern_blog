cc020a5ba85505dfacd63c374b082346d1e989c8
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-01 10:21:14 +0100

    Making a post html structue

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,21 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="post">
+        <img src="https://www.alxappliedai.com/content/images/size/w600/2023/11/DALL-E-2023-11-22-12.09.42---Create-a-3_2-aspect-ratio--high-resolution-header-image-for-an-educational-project-titled--Creating-a-Deepfake-Video---The-Trailer-.-The-image-should-.png" alt="" />
+        <h2>Creating a Deepfake Video - The Trailer</h2>
+        <p>Explore deepfake tech powered by AI. Craft unique content responsibly using free AI tools</p>
+      </div>
+      <div className="post">
+        <img src="https://www.alxappliedai.com/content/images/size/w600/2023/11/DALL-E-2023-11-22-12.09.42---Create-a-3_2-aspect-ratio--high-resolution-header-image-for-an-educational-project-titled--Creating-a-Deepfake-Video---The-Trailer-.-The-image-should-.png" alt="" />
+        <h2>Creating a Deepfake Video - The Trailer</h2>
+        <p>Explore deepfake tech powered by AI. Craft unique content responsibly using free AI tools</p>
+      </div>
+      <div className="post">
+        <img src="https://www.alxappliedai.com/content/images/size/w600/2023/11/DALL-E-2023-11-22-12.09.42---Create-a-3_2-aspect-ratio--high-resolution-header-image-for-an-educational-project-titled--Creating-a-Deepfake-Video---The-Trailer-.-The-image-should-.png" alt="" />
+        <h2>Creating a Deepfake Video - The Trailer</h2>
+        <p>Explore deepfake tech powered by AI. Craft unique content responsibly using free AI tools</p>
+      </div>
     </main>
   );
 }
