736c1f5b584069a85ddf33757dec1b2133cc13a1
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-01 17:22:47 +0100

    creating route for login

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className="post">
+        <div className="image">
+          <img src="https://www.alxappliedai.com/content/images/size/w600/2023/11/DALL-E-2023-11-22-12.09.42---Create-a-3_2-aspect-ratio--high-resolution-header-image-for-an-educational-project-titled--Creating-a-Deepfake-Video---The-Trailer-.-The-image-should-.png" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Creating a Deepfake Video - The Trailer</h2>
+          <p className="info">
+            <a className="author">Ilori Muideen</a>
+            <time>2024-04-01 15:42</time>
+          </p>
+          <p className="summary">Explore deepfake tech powered by AI. Craft unique content responsibly using free AI tools</p>
+        </div>
+      </div>
+    );
+}
