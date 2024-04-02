57c08faba5f935a683af1d4d7d85e5995c95b36a
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-02 04:48:09 +0100

    Classname attributed added for styling

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,11 +1,9 @@
 export default function LoginPage() {
     return(
-        <div>
-            <form action="">
-                <input type="text" placeholder="username"/>
-                <input type="password" placeholder="password"/>
-                <button>Login</button>
-            </form>
-        </div>
+        <form className="login">
+            <input type="text" placeholder="username"/>
+            <input type="password" placeholder="password"/>
+            <button>Login</button>
+        </form>
     );
 }
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,11 +1,9 @@
 export default function RegisterPage() {
     return (
-    <div>
-        <form action="">
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <button>Register</button>
-        </form>
-    </div>
+    <form className="register">
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+        <button>Register</button>
+    </form>
     );
 }
