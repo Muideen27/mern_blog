f14f9c28ef66b0f6b2979081a51100e14432f79e
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-02 04:32:11 +0100

    LoginPage in composition

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import Header from './Header';
 import {Route, Routes} from "react-router-dom"
 import Layout from './Layout';
 import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element= {<Layout />}>
         <Route index element={<IndexPage />} />
-        <Route path={'/login'} element={<div>Login Page</div>} />
+        <Route path={'/login'} element={<LoginPage/>} />
       </Route>
     </Routes>
   );
