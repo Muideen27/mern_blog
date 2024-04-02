2dfe45e89d17d1bdb6e6b0bb09832332bb10786a
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-02 04:26:20 +0100

    Post updatted

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import Post from './Post';
 import Header from './Header';
 import {Route, Routes} from "react-router-dom"
 import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element= {<Layout />}>
-        <Route index element={<Post />} />
+        <Route index element={<IndexPage />} />
         <Route path={'/login'} element={<div>Login Page</div>} />
       </Route>
     </Routes>
