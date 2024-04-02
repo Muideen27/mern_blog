03bf2c2ef0c804fa0713dda977226e37078ebd61
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-02 03:31:51 +0100

    Using Layout to reduce num of code with outlet

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,15 @@ import './App.css';
 import Post from './Post';
 import Header from './Header';
 import {Route, Routes} from "react-router-dom"
+import Layout from './Layout';
 
 function App() {
   return (
     <Routes>
-      <Route index element={
-            <main>
-              <Header /> 
-              <Post />
-              <Post />
-              <Post />
-          </main>
-      } />
-       <Route path={'/login'} element={
-            <main>
-              <Header /> 
-              <div>Login Page</div>
-          </main>
-       } />
+      <Route path='/' element= {<Layout />}>
+        <Route index element={<Post />} />
+        <Route path={'/login'} element={<div>Login Page</div>} />
+      </Route>
     </Routes>
   );
 }
