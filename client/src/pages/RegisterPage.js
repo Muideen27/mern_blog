d010a8e1a6de0f9a49d6a5c3f40fa8b6ef467044
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-02 17:30:45 +0100

    Create register onSubmit function

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -4,8 +4,17 @@ export default function RegisterPage() {
     const [username, setUsername] = useState('');
     const [password, setPassowrd] = useState('');
 
+    async function register(ev) {
+        ev.preventDefault;
+        await fetch('http://localhost:4000', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-type':'application/json'},
+        })
+    }
+
     return (
-    <form className="register">
+    <form className="register" onSubmit={register}>
         <h1>Register</h1>
         <input  type="text" 
                 placeholder="username"
