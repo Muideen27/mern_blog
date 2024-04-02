6a93193475b1acddb3b946c4a4849d6a8ede96ad
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-02 17:22:53 +0100

    Setting useState for username and password

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassowrd] = useState('');
+
     return (
     <form className="register">
         <h1>Register</h1>
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
+        <input  type="text" 
+                placeholder="username"
+                value={username}
+                onChange={ev => setUsername(ev.target.value)}/>
+        <input  type="password" 
+                placeholder="password"
+                value={password}
+                onChange={ev => setPassowrd(ev.target.value)}/>
         <button>Register</button>
     </form>
     );
