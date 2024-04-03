340c39845843f75c88394b10bf215208e4d943e4
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-03 13:26:29 +0100

    Error handling try catch for same user

--- api/index.js
+++ api/index.js
@@ -12,8 +12,12 @@ mongoose.connect('mongodb+srv://blog:tJ8q0dvdXBBTVyiY@blog.mdslktc.mongodb.net/?
 
 app.post('/register', async (req,res) =>{
     const {username, password} = req.body;
-    const userDoc= await User.create({username, password})
-    res.json(userDoc);
+    try{
+        const userDoc= await User.create({username, password})
+        res.json(userDoc);
+    }catch(e) {
+        res.status(400).json(e);
+    }
 });
 
 app.listen(4000);
--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const {Schema, model} = mongoose;
 
-
 const UserScheme = new Schema({
     username: {type: String, required: true, min: 4, unique: true},
     password: {type: String, required: true}
