e8c300110c56a938a5a6b7fc6a7d00c066754243
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-03 12:00:29 +0100

    Connecting mongodb

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://blog:tJ8q0dvdXBBTVyiY@blog.mdslktc.mongodb.net/?retryWrites=true&w=majority&appName=blog')
+//mongodb+srv://blog:<password>@blog.mdslktc.mongodb.net/
 
-app.post('/register', (req,res) =>{
+app.post('/register', async (req,res) =>{
     const {username, password} = req.body;
-    res.json({requestData:{username, password}});
+    const userDoc= await User.create({username, password})
+    res.json(userDoc);
 });
 
 app.listen(4000);
