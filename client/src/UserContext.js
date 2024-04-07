482729b9433775faf1deb7db19bb4d66842e4f91
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-07 21:58:20 +0100

    User content has been created

---
+++ client/src/UserContext.js
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const userContext = createContext({});
+
+export function UserContextProvider({children}) {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
