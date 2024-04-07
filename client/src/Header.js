719108d12f207d085977eec76d9ec6c6cd53353e
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-07 23:33:12 +0100

    useState debugging completed

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import {Link} from "react-router-dom";
 import { UserContext } from "./UserContext";
 
 export default function Header() {
-  const {setUserInfo, userInfo} = userContext(UserContext);
+  const {setUserInfo, userInfo} = useContext(UserContext);
   useEffect(() => {
     fetch('http://localhost:4000/profile', {
       credentials: 'include',
