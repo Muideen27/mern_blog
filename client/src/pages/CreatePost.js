52cb798a730f1dccedaec70e858a6b8632f7dc97
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-09 15:27:13 +0100

    Import react quill from react and the styling

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,9 +1,14 @@
+import ReactQuill from "react-quill"
+import "react-quill/dist/quill.snow.css"
+
 export default function CreatePost() {
     return (
         <div>
             <form action="">
                 <input type="title" placeholder={"Title"} />
                 <input type="summary" placeholder={"Summary"} />
+                <input type="file" />
+                <ReactQuill />
             </form>
         </div>
     )
