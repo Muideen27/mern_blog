cb1221b35574a9045379221ceea0b15113dc7db4
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-09 15:17:53 +0100

    Creating porm page for create new post

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -3,7 +3,7 @@ export default function CreatePost() {
         <div>
             <form action="">
                 <input type="title" placeholder={"Title"} />
-                
+                <input type="summary" placeholder={"Summary"} />
             </form>
         </div>
     )
