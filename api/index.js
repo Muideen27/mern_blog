cd5c42c0b08da0070db1e76e7016bd4c653c052c
Muideen27 <ilorimuideen0000@gmail.com>
2024-04-11 22:47:32 +0100

    CREATING THE ENDPOINT WITH MULTER

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
@@ -58,6 +59,10 @@ app.get('/profile', (req, res) => {
 
 app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok');
+});
+
+app.post('/post', (req, res) => {
+
 });
 
 app.listen(4000);
